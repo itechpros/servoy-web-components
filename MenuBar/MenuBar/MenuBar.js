cf630ba8f37834ac10ffef50ff983eff337b3604
sheinin <sergei.sheinin@gmail.com>
2018-01-05 21:00:06 +0200

    Navigation, tests&dev

--- MenuBar/MenuBar/MenuBar.js
+++ MenuBar/MenuBar/MenuBar.js
@@ -5,6 +5,31 @@ angular.module('menubarMenuBar',['servoy']).directive('menubarMenuBar', function
     	  model: '=svyModel'
       },
       controller: function($scope, $element, $attrs) {
+    	  
+    	  var container = $('#navigation')
+		  
+		  console.log(container)
+		  
+		  var a,b,c,d = document.createElement('div')
+
+		  $(d).addClass('nav-menus-wrapper')
+$(container).append(d)
+a=document.createElement('ul')
+$(a).addClass('nav-menu')
+$(d).append(a)
+b=document.createElement('li')
+//$(b).addClass('active')
+$(b).attr('class','active')
+$(a).append(b)
+
+c=document.createElement('a')
+$(c).html('Home')
+// ! no - $(c).attr('html','Home')
+$(c).attr('href','http://ya.ru')
+$(b).append(c)
+
+
+		  
       },
       templateUrl: 'menubar/MenuBar/MenuBar.html'
     };
