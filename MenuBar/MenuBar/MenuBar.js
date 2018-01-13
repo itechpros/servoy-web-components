88b1de84048f27fe2ce5014b3bdd7d0d95faba28
sheinin <sergei.sheinin@gmail.com>
2018-01-13 21:30:15 +0700

    NavBar

--- MenuBar/MenuBar/MenuBar.js
+++ MenuBar/MenuBar/MenuBar.js
@@ -5,11 +5,31 @@ angular.module('menubarMenuBar',['servoy']).directive('menubarMenuBar', function
     	  model: '=svyModel'
       },
       controller: function($scope, $element, $attrs) {
-    	  
     	  var container = $('#navigation')
-		  
-		  console.log(container)
-		  
+		  var obj={div:{class:'nav-menus-wrapper',ul:{class:'nav-menu',nodes:[{li:{class:'active',a:{html:'Home',href:'http://google.com'}}},{li:{a:{html:'Away',href:'./'}}}]}}},
+		  stack=[],count=0
+		      iter=function(_obj,_box){var a,b
+		    	  //console.log(_box)//;return
+		    	                 for(a in _obj){console.log(_obj,a,(_obj[a].constructor===Array))
+		    	                	 if(_obj[a].constructor===Array){
+		    	                		 console.log(_obj[a],_obj[a].length);
+		    	                		 //return
+		    	                	 for (var c=0;c<_obj[a].length;c+=1){
+		    	                		 console.log(_obj[a][c]);iter(_obj[a][c],_box)}}
+		    	                 else if(typeof _obj[a]==='string') if (a==='html') $(_box).html(_obj[a])
+		    	                                                                     else $(_box).attr(a,_obj[a])
+		    	                               else {
+		    	                            	   b=document.createElement(a)
+												   $(_box).append(b)
+												   
+		    	                            	   iter(_obj[a],b)
+		    	                               }   }}
+    //	 var d =document.createElement('div')
+		// d.style.backgroundColor='red'
+    	 //$(d).addClass('nav-menus-wrapper')
+		   // 	                               $(container).append(d)
+		    	                               iter(obj,container)
+		  /*
 		  var a,b,c,d = document.createElement('div')
 
 		  $(d).addClass('nav-menus-wrapper')
@@ -24,10 +44,10 @@ $(a).append(b)
 
 c=document.createElement('a')
 $(c).html('Home')
-// ! no - $(c).attr('html','Home')
+//$(c).attr('html','Home1')
 $(c).attr('href','http://ya.ru')
 $(b).append(c)
-
+*/
 
 		  
       },
