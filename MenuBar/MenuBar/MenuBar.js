a569d16a13e39febbe5305de612a2e2c0fc7d003
sheinin <sergei.sheinin@gmail.com>
2018-01-19 22:40:46 +0700

    Mega Nav Menu Web Component

--- MenuBar/MenuBar/MenuBar.js
+++ MenuBar/MenuBar/MenuBar.js
@@ -1,55 +1,29 @@
 angular.module('menubarMenuBar',['servoy']).directive('menubarMenuBar', function() {  
-    return {
+  return {
       restrict: 'E',
       scope: {
-    	  model: '=svyModel'
+          api: '=svyApi',
+          model: '=svyModel'
       },
       controller: function($scope, $element, $attrs) {
-    	  var container = $('#navigation')
-		  var obj={div:{class:'nav-menus-wrapper',ul:{class:'nav-menu',nodes:[{li:{class:'active',a:{html:'Home',href:'http://google.com'}}},{li:{a:{html:'Away',href:'./'}}}]}}},
-		  stack=[],count=0
-		      iter=function(_obj,_box){var a,b
-		    	  //console.log(_box)//;return
-		    	                 for(a in _obj){console.log(_obj,a,(_obj[a].constructor===Array))
-		    	                	 if(_obj[a].constructor===Array){
-		    	                		 console.log(_obj[a],_obj[a].length);
-		    	                		 //return
-		    	                	 for (var c=0;c<_obj[a].length;c+=1){
-		    	                		 console.log(_obj[a][c]);iter(_obj[a][c],_box)}}
-		    	                 else if(typeof _obj[a]==='string') if (a==='html') $(_box).html(_obj[a])
-		    	                                                                     else $(_box).attr(a,_obj[a])
-		    	                               else {
-		    	                            	   b=document.createElement(a)
-												   $(_box).append(b)
-												   
-		    	                            	   iter(_obj[a],b)
-		    	                               }   }}
-    //	 var d =document.createElement('div')
-		// d.style.backgroundColor='red'
-    	 //$(d).addClass('nav-menus-wrapper')
-		   // 	                               $(container).append(d)
-		    	                               iter(obj,container)
-		  /*
-		  var a,b,c,d = document.createElement('div')
+          var container = $('#navigation')
+          
+          $(document).ready(function()
+          {  
+              if ($scope.model.menu)
+              {
+                 container.html($scope.model.menu)
+                 container.navigation()
+		      }
+          })
+              
+          $scope.api.setMenu = function(menu)
+          {
+              container.html(menu)
+              container.navigation()
+              return true
+          }
 
-		  $(d).addClass('nav-menus-wrapper')
-$(container).append(d)
-a=document.createElement('ul')
-$(a).addClass('nav-menu')
-$(d).append(a)
-b=document.createElement('li')
-//$(b).addClass('active')
-$(b).attr('class','active')
-$(a).append(b)
-
-c=document.createElement('a')
-$(c).html('Home')
-//$(c).attr('html','Home1')
-$(c).attr('href','http://ya.ru')
-$(b).append(c)
-*/
-
-		  
       },
       templateUrl: 'menubar/MenuBar/MenuBar.html'
     };
--- MenuBar/MenuBar/MenuBar.spec
+++ MenuBar/MenuBar/MenuBar.spec
@@ -3,9 +3,45 @@
 	"displayName": "MenuBar",
 	"version": 1,
 	"definition": "menubar/MenuBar/MenuBar.js",
-	"libraries": [],
+	"libraries": [
+		{
+			"name": "navigation.css",
+			"version": "1",
+			"mimetype": "text/css",
+			"url": "menubar/MenuBar/lib/css/navigation.css"
+		},
+		{
+            "name": "ionicons.min.css",
+            "version": "1",
+            "mimetype": "text/css",
+            "url": "menubar/MenuBar/ionicons-2.0.1/css/ionicons.min.css"
+        },
+		{
+			"name": "navigation.js",
+			"version": "1",
+			"mimetype": "text/javascript",
+			"url": "menubar/MenuBar/lib/js/navigation.js"
+    	}
+	],
 	"model":
 	{
-		"yourName" : {"type": "string", "default":"World"}
+		"menu": "string"
+	},
+	"api":
+	{
+		"setMenu": 
+        {
+            "returns": "boolean",
+            "parameters":
+                [
+                    {
+                        "name": "menu",
+                        "type": "string"
+                    }
+                ]
+        }
+		
 	}
+	
+	
 }
