200960dc4faf535a91cbc0cde07deafb57075637
sheinin <sergei.sheinin@gmail.com>
2019-08-08 15:02:13 +0700

    Datepicker Pro

---
+++ calendarpro/calendarPro/calendar.js
@@ -0,0 +1,364 @@
+angular.module('itechCalendarPro', [ 'servoy' ]).directive('itechCalendarPro', function($log, $apifunctions, $svyProperties, $sabloConstants,$sabloApplication,$applicationService,$animate) {
+	return {
+		restrict : 'E',
+		scope : {
+			model : "=svyModel",
+			handlers : "=svyHandlers",
+			api : "=svyApi",
+			svyServoyapi : "="
+		},
+		link : function($scope, $element, $attrs) {
+			if($attrs['svyPortalCell']) {
+				$animate.enabled($element, false);
+			}
+			var child = $element.children();
+			var ngModel = child.controller("ngModel");
+			var isDataFormatted = true;
+
+			var options = {
+					widgetParent: $(document.body),
+					useCurrent : false,
+					useStrict : true,
+					showClear : true,
+					ignoreReadonly : true,
+					showTodayButton: true,
+					calendarWeeks: true,
+					showClose: true,
+					icons: {
+						close: 'glyphicon glyphicon-ok'
+					}
+			,weekstart:3
+				};
+			// rely on servoy_app.js setLocale which searches the correct locale to set
+			options.locale = numeral.locale();
+			var showISOWeeks = $applicationService.getUIProperty('ngCalendarShowISOWeeks');
+			if (showISOWeeks)
+			{
+				options.isoCalendarWeeks = true;
+			}	
+			
+			child.datetimepicker(options);
+			var theDateTimePicker = child.data('DateTimePicker');
+			
+			console.log(child,$element,theDateTimePicker,$('.datepicker'))//.datepicker({weekStart: 3, format:'mm/dd/yyyy'}))
+//theDateTimePicker.weekStart = 3
+			function inputChanged(e) {
+				if ($scope.model.findmode) {
+					ngModel.$setViewValue(child.children("input").val());
+				} else {
+					if (e.date)
+						ngModel.$setViewValue(e.date.toDate());
+					else
+						ngModel.$setViewValue(null);
+				}
+				ngModel.$setValidity("", true);
+				$scope.svyServoyapi.apply('dataProviderID');
+			};
+			
+			function correctDateValueToUse(newValue) {
+				// .date() throws exception if (newDate !== null && typeof newDate !== 'string' && !moment.isMoment(newDate) && !(newDate instanceof Date))
+				// because we do call this method quite fast using ngModel$viewValue that value might be NaN (used by angular JS) because ngModel code only
+				// initializes it to the actual value in a watch so later; we also want to be able to call .date() for undefined values
+				return (angular.isDefined(newValue) && !isNaN(newValue)) ? newValue : null;
+			}
+
+			// when model change, update our view, set the date in the
+			// datepicker
+			ngModel.$render = function() {
+				try {
+					$element.off("dp.change", inputChanged);
+					if (theDateTimePicker && !$scope.model.findmode) theDateTimePicker.date(correctDateValueToUse(ngModel.$viewValue)); // set default date for widget open; turn undefined to null as well (undefined gives exception)
+					else {
+						// in find mode
+						child.children("input").val(ngModel.$viewValue);
+					}
+				} finally {
+					$element.on("dp.change", inputChanged);
+				}
+			};
+
+			var dateFormat = 'YYYY-MM-DD';
+
+			/**
+			 * detect IE returns true if browser is IE or false, if browser is
+			 * not Internet Explorer
+			 */
+			function detectIE() {
+				var ua = window.navigator.userAgent;
+
+				var msie = ua.indexOf('MSIE ');
+				var trident = ua.indexOf('Trident/');
+				var edge = ua.indexOf('Edge/');
+				if (msie > 0 || trident > 0 || edge > 0) {
+					return true;
+				}
+
+				// other browser
+				return false;
+			}
+
+			// helper function
+			function setDateFormat(format, which) {
+				if (!isDataFormatted)
+					return;
+				$element.off("dp.change", inputChanged);
+				if (format && format[which]) {
+					dateFormat = moment().toMomentFormatString(format[which]);
+				}
+
+				if (angular.isDefined(theDateTimePicker)) { // can be undefined in find mode
+					var ieVersion = detectIE();
+					var start=0;
+					var end=0;
+					if(ieVersion){
+
+						start = child.children("input")[0].selectionStart;
+						end = child.children("input")[0].selectionEnd;
+					}
+					theDateTimePicker.format(dateFormat);
+
+					try {
+						theDateTimePicker.date(correctDateValueToUse(ngModel.$viewValue));
+
+					}
+					finally {
+						var elem = document.getElementById($scope.model.svyMarkupId);
+						if(start > 0){
+							if (elem.setSelectionRange) {
+								isDataFormatted = true;
+								elem.setSelectionRange(start, end);
+								isDataFormatted = false;
+							} else if (elem.createTextRange) {
+							isDataFormatted = true;
+							var selRange = elem.createTextRange();
+							selRange.collapse(true);
+							selRange.moveStart('character', start);
+							selRange.moveEnd('character', end);
+							selRange.select();
+							isDataFormatted = false;
+						} else if (typeof start != 'undefined') {
+							isDataFormatted = true;
+							elem.selectionStart = start;
+							elem.selectionEnd = end;
+							isDataFormatted = false;
+							}
+						}
+						$element.on("dp.change",inputChanged);
+					}
+				}
+			}
+
+			$element.on("dp.change", inputChanged);
+
+			function onError(val) {
+				if (child.children("input").val() === '') {
+					ngModel.$setViewValue(null);
+					ngModel.$setValidity("", true);
+					$scope.svyServoyapi.apply('dataProviderID');
+					return;
+				} else if (!moment(child.children("input").val()).isValid) {
+					ngModel.$setValidity("", false);
+				}
+				$scope.$digest();
+			}
+			$element.on("dp.error", onError);
+
+			$scope.$watch('model.findmode', function() {
+				if ($scope.model.findmode) {
+					theDateTimePicker.destroy();
+				} else {
+					$element.off("dp.error");
+					var opts = {
+								widgetParent: $(document.body),
+								useCurrent : false,
+								useStrict : true,
+								showClear : true,
+								ignoreReadonly : true,
+								showTodayButton: true,
+								calendarWeeks: true,
+								showClose: true,
+								icons: {
+									close: 'glyphicon glyphicon-ok'
+								}
+							};
+					if (showISOWeeks)
+					{
+						opts.isoCalendarWeeks = true;
+					}	
+					child.datetimepicker(opts);
+					theDateTimePicker = child.data('DateTimePicker');
+					theDateTimePicker.format(dateFormat);
+					try {
+						$element.off("dp.change", inputChanged);
+						theDateTimePicker.date(correctDateValueToUse(ngModel.$viewValue));
+					} finally {
+						$element.on("dp.error", onError);
+						$element.on("dp.change", inputChanged);
+					}
+				}
+			});
+
+			var storedTooltip = false;
+			$scope.api.onDataChangeCallback = function(event, returnval) {
+				var stringValue = typeof returnval == 'string'
+				if (returnval === false || stringValue) {
+					$element[0].focus();
+					ngModel.$setValidity("", false);
+					if (stringValue) {
+						if (storedTooltip == false)
+							storedTooltip = $scope.model.toolTipText;
+						$scope.model.toolTipText = returnval;
+					}
+				} else {
+					ngModel.$setValidity("", true);
+					if (storedTooltip !== false)
+						$scope.model.toolTipText = storedTooltip;
+					storedTooltip = false;
+				}
+			}
+
+			$scope.focusGained = function(event) {
+				if (!$scope.model.findmode) {
+					if ($scope.model.format.edit && $scope.model.format.isMask) {
+						var settings = {};
+						settings.placeholder = $scope.model.format.placeHolder ? $scope.model.format.placeHolder : " ";
+						if ($scope.model.format.allowedCharacters)
+							settings.allowedCharacters = $scope.model.format.allowedCharacters;
+
+						$element.find('input').mask($scope.model.format.edit, settings);
+						$element.on("dp.change", inputChanged);
+					}
+					else if ($scope.model.format.edit)
+						setDateFormat($scope.model.format, 'edit');
+					else
+						setDateFormat($scope.model.format, 'display');
+				}
+			}
+
+			$scope.focusLost = function(event) {
+				if (!$scope.model.findmode) {
+					if ($scope.model.format.edit && $scope.model.format.isMask)
+					{
+						$element.find('input').unmask();
+					}
+					else
+					{
+						setDateFormat($scope.model.format, 'display');
+					}
+				}
+				else
+				{
+					inputChanged(event);
+				}	
+			}
+
+			/**
+			 * Set the focus to this calendar.
+			 * 
+			 * @example %%prefix%%%%elementName%%.requestFocus();
+			 * @param mustExecuteOnFocusGainedMethod
+			 *            (optional) if false will not execute the onFocusGained
+			 *            method; the default value is true
+			 */
+			$scope.api.requestFocus = function(mustExecuteOnFocusGainedMethod) {
+				var input = $element.find('input');
+				if (mustExecuteOnFocusGainedMethod === false && $scope.handlers.onFocusGainedMethodID) {
+					input.unbind('focus');
+					input[0].focus();
+					input.bind('focus', $scope.handlers.onFocusGainedMethodID)
+				} else {
+					input[0].focus();
+				}
+			}
+
+			$scope.api.getWidth = $apifunctions.getWidth($element[0]);
+			$scope.api.getHeight = $apifunctions.getHeight($element[0]);
+			$scope.api.getLocationX = $apifunctions.getX($element[0]);
+			$scope.api.getLocationY = $apifunctions.getY($element[0]);
+
+			var element = $element.children().first();
+			var inputElement = element.children().first();
+			var className = null;
+			Object.defineProperty($scope.model, $sabloConstants.modelChangeNotifier, {
+				configurable : true,
+				value : function(property, value) {
+					switch (property) {
+					case "borderType":
+						$svyProperties.setBorder(element, value);
+						break;
+					case "margin":
+						if (value)
+							element.css(value);
+						break;
+					case "background":
+					case "transparent":
+						$svyProperties.setCssProperty(inputElement, "backgroundColor", $scope.model.transparent ? "transparent" : $scope.model.background);
+						break;
+					case "foreground":
+						$svyProperties.setCssProperty(inputElement, "color", value);
+						break;
+					case "selectOnEnter":
+						if (value)
+							$svyProperties.addSelectOnEnter(inputElement);
+						break;
+					case "fontType":
+						$svyProperties.setCssProperty(inputElement, "font", value);
+						break;
+					case "enabled":
+						if (value)
+							inputElement.removeAttr("disabled");
+						else
+							inputElement.attr("disabled", "disabled");
+						break;
+					case "editable":
+						if (value && !$scope.model.readOnly)
+							inputElement.removeAttr("readonly");
+						else
+							inputElement.attr("readonly", "readonly");
+						break;
+					case "readOnly":
+						if (!value && $scope.model.editable)
+							inputElement.removeAttr("readonly");
+						else
+							inputElement.attr("readonly", "readonly");
+						break;	
+					case "horizontalAlignment":
+						$svyProperties.setHorizontalAlignment(inputElement, value);
+						break;
+					case "rolloverCursor":
+						element.css('cursor', value == 12 ? 'pointer' : 'default');
+						break;
+					case "size":
+						$svyProperties.setCssProperty(inputElement, "height", value.height);
+						break;
+					case "styleClass":
+						if (className)
+							inputElement.removeClass(className);
+						className = value;
+						if (className)
+							inputElement.addClass(className);
+						break;
+					case "format":
+						setDateFormat($scope.model.format, 'display');
+						break;
+					}
+				}
+			});
+			$svyProperties.createTooltipState(element, function() { return $scope.model.toolTipText });
+			var destroyListenerUnreg = $scope.$on("$destroy", function() {
+				if (angular.isDefined(theDateTimePicker)) { // can be undefined in find mode
+					theDateTimePicker.destroy();
+				}
+				destroyListenerUnreg();
+				delete $scope.model[$sabloConstants.modelChangeNotifier];
+			});
+			// data can already be here, if so call the modelChange function so
+			// that it is initialized correctly.
+			var modelChangFunction = $scope.model[$sabloConstants.modelChangeNotifier];
+			for (var key in $scope.model) {
+				modelChangFunction(key, $scope.model[key]);
+			}
+		},
+		templateUrl : 'servoydefault/calendar/calendar.html'
+	};
+})
---
+++ calendarpro/calendarPro/calendar.spec
@@ -0,0 +1,128 @@
+{
+	"name": "itech-calendar-pro",
+	"displayName": "Calendar Pro",
+	"version": 1,
+	"icon": "calendarpro/calendarPro/calendar.png",
+	"definition": "calendarpro/calendarPro/calendar.js",
+	"libraries": [
+			{"name":"moment", "version":"2.19.1", "url": "calendarpro/calendarPro/bootstrap-datetimepicker/js/moment-with-locales.min.js", "mimetype":"text/javascript"},
+			{"name":"moment-jdateformatparser", "version":"0.1.1", "url":"calendarpro/calendarPro/bootstrap-datetimepicker/js/moment-jdateformatparser.js", "mimetype":"text/javascript"},
+			{"name":"bootstrap-datetimepicker", "version":"4.7.47", "url":"calendarpro/calendarPro/bootstrap-datetimepicker/js/bootstrap-datetimepicker.js", "mimetype":"text/javascript"},
+			{"name":"bootstrap-datetimepicker", "version":"4.7.47", "url":"calendarpro/calendarPro/bootstrap-datetimepicker/css/bootstrap-datetimepicker.css", "mimetype":"text/css"},
+			{"name":"bootstrap-datetimepicker-servoy-extension", "version":"1.0", "url":"calendarpro/calendarPro/bootstrap-datetimepicker/css/bootstrap-datetimepicker-servoy-extension.css", "mimetype":"text/css"}
+		],
+	"model":
+	{
+	        "background" : "color", 
+	        "borderType" : {"type":"border","stringformat":true}, 
+	        "dataProviderID" : { "type":"dataprovider", "pushToServer": "allow", "tags": { "scope": "design" }, "ondatachange": { "onchange":"onDataChangeMethodID", "callback":"onDataChangeCallback"}, "displayTagsPropertyName" : "displaysTags"}, 
+	        "displaysTags" : { "type" : "boolean", "tags": { "scope" : "design" } }, 
+	        "editable" : {"type":"boolean", "default":true}, 
+	        "enabled" : { "type": "enabled", "blockingOn": false, "default": true, "for": ["dataProviderID","onActionMethodID","onDataChangeMethodID","onFocusGainedMethodID","onFocusLostMethodID","onRightClickMethodID"] }, 
+	        "findmode" : { "type":"findmode", "tags":{"scope":"private"}, "for" : {"editable":true}}, 
+	        "fontType" : {"type":"font","stringformat":true}, 
+	        "foreground" : "color", 
+	        "format" : {"for":["dataProviderID"] , "type" :"format"}, 
+	        "horizontalAlignment" : {"type" :"int", "tags": { "scope" :"design" }, "values" :[{"LEFT":2}, {"CENTER":0},{"RIGHT":4}],"default" : -1}, 
+	        "location" : {"type" :"point", "pushToServer": "deep"}, 
+	        "margin" : {"type" :"insets", "tags": { "scope" :"design" }}, 
+	        "placeholderText" : { "type" : "tagstring", "displayTagsPropertyName" : "displaysTags" }, 
+	        "readOnly" : { "type" : "protected", "for" : ["readOnly"]}, 
+	        "selectOnEnter" : {"type" :"boolean", "tags": { "scope" :"design" }}, 
+	        "size" : {"type" :"dimension",  "default" : {"width":140, "height":20}, "pushToServer": "deep"}, 
+	        "styleClass" : { "type" :"styleclass", "tags": { "scope" :"design" }, "values" :["form-control", "input-sm", "svy-padding-xs", "svy-line-height-normal"]}, 
+	        "tabSeq" : {"type" :"tabseq", "tags": { "scope" :"design" }}, 
+	        "text" : { "type" : "tagstring", "displayTagsPropertyName" : "displaysTags" }, 
+	        "toolTipText" : { "type" : "tagstring", "displayTagsPropertyName" : "displaysTags" }, 
+	        "transparent" : "boolean", 
+	        "visible" : "visible" 
+	},
+	"handlers":
+	{
+	        "onActionMethodID" : {
+	         	
+	        	"parameters":[
+								{
+						          "name":"event",
+								  "type":"JSEvent"
+								} 
+							 ]
+	        }, 
+	        "onDataChangeMethodID" : {
+	          "returns": "boolean", 
+	         	
+	        	"parameters":[
+								{
+						          "name":"oldValue",
+								  "type":"${dataproviderType}"
+								}, 
+								{
+						          "name":"newValue",
+								  "type":"${dataproviderType}"
+								}, 
+								{
+						          "name":"event",
+								  "type":"JSEvent"
+								} 
+							 ]
+	        }, 
+	        "onFocusGainedMethodID" : {
+	         	
+	        	"parameters":[
+								{
+						          "name":"event",
+								  "type":"JSEvent"
+								} 
+							 ]
+	        }, 
+	        "onFocusLostMethodID" : {
+	         	
+	        	"parameters":[
+								{
+						          "name":"event",
+								  "type":"JSEvent"
+								} 
+							 ]
+	        }, 
+	        "onRightClickMethodID" : {
+	         	
+	        	"parameters":[
+								{
+						          "name":"event",
+								  "type":"JSEvent"
+								} 
+							 ]
+	        } 
+	},
+	"api":
+	{
+	        "getFormName": {
+	            "returns": "string"
+	        },
+	        "getHeight": {
+	            "returns": "int"
+	        },
+	        "getLocationX": {
+	            "returns": "int"
+	        },
+	        "getLocationY": {
+	            "returns": "int"
+	        },
+	        "getWidth": {
+	            "returns": "int"
+	        },
+	        "requestFocus": {
+				"parameters":[
+								{
+ 								"name":"mustExecuteOnFocusGainedMethod",
+								"type":"boolean",
+			            		"optional":true
+			            		}
+							 ],
+				"delayUntilFormLoads": true,
+			"discardPreviouslyQueuedSimilarCalls": true
+
+	        }
+	}
+	
+}
