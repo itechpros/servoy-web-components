977691390310b0828fd73e89a79d16d0fd5c9882
ssheinin <sergei.sheinin@gmail.com>
2020-01-29 13:03:58 +0700

    enableTabs() in itechutilsDOM

--- itechutils/DOM/DOM.js
+++ itechutils/DOM/DOM.js
@@ -67,6 +67,22 @@ angular.module('itechutilsDOM',['servoy'])
 		         
 		         
 		      },
+			  enableTabs: function(selector) {
+
+				  var textareas = document.getElementsByClassName(selector)
+				  var count = textareas.length
+				  for(var i=0;i<count;i++){
+				      textareas[i].onkeydown = function(e){
+				          if(e.keyCode==9 || e.which==9){
+				              e.preventDefault();
+				              var s = this.selectionStart;
+				              this.value = this.value.substring(0,this.selectionStart) + "\t" + this.value.substring(this.selectionEnd);
+				              this.selectionEnd = s+1 
+				          }
+				      }
+				  }
+
+			  }
 	}
 })
 .run(function($rootScope,$services)
--- itechutils/DOM/DOM.spec
+++ itechutils/DOM/DOM.spec
@@ -63,6 +63,16 @@
                "type":"string"
             }
          ]
+      },
+      "enableTabs":
+      {
+          "parameters":
+          [
+              {
+                 "name":"selector",
+                 "type":"string"
+              }
+          ]
       }
  	}
 }
